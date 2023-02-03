7d17088818114badd200e021d73e5ad20d6026a1
Al Fitrah Ramadhan <121604634+pitrahramdani@users.noreply.github.com>
2023-02-03 17:07:50 +0700

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-const menuBar = document.querySelector("nav #menu-bar");
-const menuToggle = document.querySelector(".menu-toggle input");
-
-menuToggle.addEventListener("click", function () {
-  menuBar.classList.toggle("slide");
-});
